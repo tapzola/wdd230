f9c3439d0de1236e0e0db3362c0b83435fa7a588
tapzola <tapelo.dbe@gmail.com>
2022-09-17 20:33:26 -0600

    final creation of website

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+
+// get the current full date
+const todayDate = new Date();
+
+// get the current year
+const currYear = todayDate.getFullYear();
+
+// combine the copyright sign with the curr year
+document.querySelector("#fullYear").textContent = currYear;
+
+//get the updated date and time
+
+document.querySelector("#lastUpdated").textContent = `Last Updated: ${document.lastModified}`;
