e0e4f4d70dbcde767dbf155dcf5bc694256b5152
tapzola <tapelo.dbe@gmail.com>
2022-09-24 02:12:14 -0600

    finish ranch

---
+++ ranch/scripts/main.js
@@ -0,0 +1,8 @@
+// get the current full date
+const todayDate = new Date();
+
+// get the current year
+const currYear = todayDate.getFullYear();
+
+// combine the copyright sign with the curr year
+document.querySelector("#fullYear").textContent = currYear;
