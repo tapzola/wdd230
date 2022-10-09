75ea615d0b93f36a957dbd7b1be2abae5325acd9
tapzola <tapelo.dbe@gmail.com>
2022-10-08 23:35:43 -0600

    chamber home build

---
+++ chamber/js/main.js
@@ -0,0 +1,48 @@
+// select the elements to manipulate (output to)
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+
+
+
+// get the current full date
+const todayDate = new Date();
+
+// get the current year
+const currYear = todayDate.getFullYear();
+
+// combine the copyright sign with the curr year
+document.querySelector("#fullYear").textContent = currYear;
+
+//get the updated date and time
+
+document.querySelector("#lastUpdated").textContent = `Last Updated: ${document.lastModified}`;
+
+
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
