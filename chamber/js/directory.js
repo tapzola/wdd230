691895dc2c41f589ae30ccc67bf94782755e0d73
tapzola <tapelo.dbe@gmail.com>
2022-11-15 00:59:14 -0700

    changes

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,19 +15,30 @@ fetch(requestURL)
  
 
 function displayCompanies(company) {
+
+  const gridbutton = document.querySelector("#grid");
+  const listbutton = document.querySelector("#list");
+  const show = document.querySelector('div.cards');
+
+
+  
+
     // Create elements to add to the document
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
+    let name = document.createElement('h3');
     let image = document.createElement('img');
     let address = document.createElement('p');
     let phonenumber = document.createElement('p');
     let website = document.createElement('p');
   
+  
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${company.companyname}`;
+    name.textContent = `${company.companyname}`;
     address.textContent = `${company.address}`
     phonenumber.textContent = `${company.phonenumber}`
     website.textContent = `${company.website}`
+
+
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     image.setAttribute('src', company.image);
@@ -35,7 +46,9 @@ function displayCompanies(company) {
     image.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
+    
     card.appendChild(image);
+    card.appendChild(name)
     card.appendChild(address);
     card.appendChild(phonenumber);
     card.appendChild(website);
@@ -44,23 +57,25 @@ function displayCompanies(company) {
    
   
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
+   // document.querySelector('div.cards').appendChild(card);
+    show.appendChild(card);
+    
+    function showGrid() {
+      show.classList.add("cards");
+      show.classList.remove("list");
+    };
 
-    const gridbutton = document.querySelector("#grid");
-    const listbutton = document.querySelector("#list");
 
-    gridbutton.addEventListener("click", () => {
-      // example using arrow function
-      card.classList.add("cards");
-      card.classList.remove("list");
-    });
+    gridbutton.addEventListener("click", showGrid);
+
+    function showList() {
+      show.classList.add("list");
+      show.classList.remove("cards");
+    }
 
     listbutton.addEventListener("click", showList); // example using defined function
 
-    function showList() {
-      card.classList.add("list");
-      card.classList.remove("cards");
-    };
+    
   };
 
 
