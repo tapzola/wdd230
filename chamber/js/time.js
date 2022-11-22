9eb4503e40617088cbc69df618b7427cb7846d96
tapzola <tapelo.dbe@gmail.com>
2022-11-21 23:12:21 -0700

    put time since last visit

--- chamber/js/time.js
+++ chamber/js/time.js
@@ -1,2 +1,12 @@
-const d = new Date();
-  document.querySelector("#date").textContent = d ;
+const visit = document.querySelector("#today");
+let lastVisit = window.localStorage.getItem("lastVisitTime");
+
+if (lastVisit == null) {
+  visit.textContent  = "This is your first visit";
+} else {
+  visit.textContent = `${Math.round((Date.now() - lastVisit)/ 86400000)} days since last visit`;
+}
+
+let visitTime = Date.now();
+
+localStorage.setItem("lastVisitTime", visitTime);
