f82d0096857229f8ae4595afe1070693c058e598
tapzola <tapelo.dbe@gmail.com>
2022-12-14 23:17:14 -0700

    many changes

--- final-project/js/freshpage.js
+++ final-project/js/freshpage.js
@@ -1,95 +1,107 @@
 const requestURL = 'json/data.json';
-let dropdown = document.querySelector('#first-fruit');
-let dropdown2 = document.querySelector('#sec-fruit');
-let dropdown3 = document.querySelector('#third-fruit');
+const  freshBtn = document.querySelector("#fresh-submit");
 
-dropdown.length = 0;
-dropdown2.length = 0;
-dropdown3.length = 0;
-
-let defaultOption = document.createElement('option');
-defaultOption.text = ' ';
-let defaultOption2 = document.createElement('option');
-defaultOption2.text = ' ';
-let defaultOption3 = document.createElement('option');
-defaultOption3.text = ' ';
-
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
-dropdown2.add(defaultOption2);
-dropdown2.selectedIndex = 0;
-dropdown3.add(defaultOption3);
-dropdown3.selectedIndex = 0;
-
-
-fetch(requestURL)
+fetch(requestURL) 
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  
     const fruits = jsonObject['fruits'];
 
-    let option;
-    let option2;
-    let option3;
-    
-    	for (let i = 0; i < fruits.length; i++) {
-          option = document.createElement('option');
-      	  option.text = fruits[i].name;
-      	  option.value = fruits[i].name;
-          option2 = document.createElement('option');
-      	  option2.text = fruits[i].name;
-      	  option2.value = fruits[i].name;
-          option3 = document.createElement('option');
-      	  option3.text = fruits[i].name;
-      	  option3.value = fruits[i].name;
-      	  dropdown.add(option);
-          dropdown2.add(option2);
-          dropdown3.add(option3);
+  
+   fruitSelection(fruits, '#first-fruit');
+   fruitSelection(fruits, '#sec-fruit');
+   fruitSelection(fruits, '#third-fruit');
 
-        }
-    
-        let result = document.querySelector('.result');
-        let result2 = document.querySelector('.result2');
-        let result3 = document.querySelector('.result3');
-        
-        dropdown.addEventListener('change', () => {
-            const fruits = jsonObject['fruits'];
-            result.textContent = selection.options[dropdown.selectedIndex].value;
-            let fruitOne = dropdown.selectedIndex;
-            console.log(fruitOne);
-            for (let i = 0; i < fruits.length; i++) {
-              if (fruits[i].name == fruitOne){
+ 
+   getOption();
 
-                
-                 return document.querySelector('#carbs').innerHTML = fruits[i].nutritions.carbohydrates;
-
-              }
-
-                
-            }
-
-        
-        });
-
-        dropdown2.addEventListener('change', () => {
-          result2.textContent = selection.options[dropdown2.selectedIndex].value;
-          let fruitTwo = dropdown2.selectedIndex;
-            console.log(fruitTwo);
-
-      
-      });
-
-      dropdown3.addEventListener('change', () => {
-        result3.textContent = selection.options[dropdown3.selectedIndex].value;
-        let fruitThree = dropdown3.selectedIndex;
-            console.log(fruitThree);
-
-      
-    });
-
-
-    
   });
 
+
+ function fruitSelection(fruitOptions, fruitId) {
+   let choice  = document.querySelector(fruitId)
+   for(let i in fruitOptions) {
+     let option =  document.createElement("option")
+     option.innerHTML = fruitOptions[i].name;
+     option.value = fruitOptions[i].name;
+     choice.appendChild(option);
+   }
+
+  }
+
+   
+  function getOption(fruits) {
+    const name  = document.getElementById('first-name').value;
+    const email  = document.getElementById('email').value;
+    const phone   = document.getElementById('phone-number').value;
+    const date  = document.getElementById('order-date').value;
+    const fruit1 = document.getElementById("first-fruit").value;
+    const fruit2 = document.getElementById("sec-fruit").value;
+    const fruit3 = document.getElementById("third-fruit").value;
+    const instructions = document.getElementById("instructions").value;
+
+    displayNutrients(fruits, fruit1, fruit2, fruit3);
+   displayInfo(name, email, phone, date, fruit1, fruit2, fruit3,instructions);
+    
+  }
+
+
+function displayInfo(name, email, phone, date, fruit1, fruit2, fruit3,instructions){
+  document.querySelector('.name-output').textContent =  name;
+  document.querySelector('.phone-output').textContent =  phone;
+  document.querySelector('.email-output').textContent =  email;
+  document.querySelector('.date-output').textContent =  date;
+  document.querySelector('.fruit-output1').textContent =  fruit1;
+  document.querySelector('.fruit-output2').textContent = fruit2;
+  document.querySelector('.fruit-output3').textContent = fruit3;
+  document.querySelector('.special').textContent = instructions;
+
+
+}
+
+
+function displayNutrients(fruits, fruit1, fruit2, fruit3){
+
+
+  const requestURL = 'json/data.json';
+
+
+fetch(requestURL) 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const fruits = jsonObject['fruits'];
+    let fruitData1 = fruits.filter(fruits => fruits.name == fruit1)[0];
+  let fruitData2 = fruits.filter(fruits => fruits.name == fruit2)[0];
+  let fruitData3 = fruits.filter(fruits => fruits.name == fruit3)[0];
+
+    
+  const carbohydrates = fruitData1.nutritions.carbohydrates  +  fruitData2.nutritions.carbohydrates + fruitData3.nutritions.carbohydrates;
+  const proteins  = fruitData1.nutritions.protein  +  fruitData2.nutritions.protein + fruitData3.nutritions.protein;
+  const fats = fruitData1.nutritions.fat  +  fruitData2.nutritions.fat + fruitData3.nutritions.fat;
+  const calories  = fruitData1.nutritions.calories  +  fruitData2.nutritions.calories + fruitData3.nutritions.calories;
+  const sugar =fruitData1.nutritions.sugar +  fruitData2.nutritions.sugar + fruitData3.nutritions.sugar;
+   
+  document.querySelector('.carbs').textContent = carbohydrates.toFixed(0);
+  document.querySelector('.proteins').textContent = proteins.toFixed(0);
+  document.querySelector('.fats').textContent = fats.toFixed(0);
+  document.querySelector('.sugar').textContent = sugar.toFixed(0);
+  document.querySelector('.cals').textContent = calories.toFixed(0);
+   
+  });
+
+
+
+    }
+
+const drinks =  document.getElementById('drink-num');
+let drinksNum = window.localStorage.getItem("drinksTotal");
+
+if (drinksNum == null) {
+  drinks.textContent  = "0";
+} else {
+    drinksNum ++
+    localStorage.setItem("lastVisitTime", drinksNum);
+}
